8c8b97cec778589ee9bf0d67ae8fa0039803881b
Ayush K M <kmayushkm@gmail.com>
2022-08-20 21:37:14 +0530

    Added support for headings

--- md2htmlconv/lexer.py
+++ md2htmlconv/lexer.py
@@ -3,11 +3,10 @@
 
 TODO:
 1. Support for custom tags
-2. Support for Headings
-3. Support for Mathematical equations
-4. Support for table
-5. Support for Images
-6. Support for order
+2. Support for Mathematical equations
+3. Support for table
+4. Support for Images
+5. Support for order
 
 It contains lexer which uses pygments for generating tokens. Previously
 I tried using ply for lexing but was quite complicated to implement and
@@ -24,6 +23,15 @@ class MarkdownLexer(RegexLexer):
 
 	tokens = {
 		"root": [
+
+			# Headings
+			(r"######.+", Generic.Md.Heading6),
+			(r"#####.+", Generic.Md.Heading5),
+			(r"####.+", Generic.Md.Heading4),
+			(r"###.+", Generic.Md.Heading3),
+			(r"##.+", Generic.Md.Heading2),
+			(r"#.+", Generic.Md.Heading1),
+
 
 			# Quote codes
 			(r"\`\`\`(.|\n)+\`\`\`", Generic.Md.MultilineCode),
