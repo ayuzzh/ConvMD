868ae43ef56c60e48d8d841c6a883be70796f14f
Ayush K M <kmayushkm@gmail.com>
2022-08-20 21:37:34 +0530

    Added tests

---
+++ test/test1.py
@@ -0,0 +1,40 @@
+from md2htmlconv import lexer
+
+if __name__ == "__main__":
+	l = lexer.MarkdownLexer()
+	text = """
+`Single line code`
+
+```
+Multiline code
+
+code
+```
+
+line break<br>
+
+text
+
+> Quote
+
+<sub> subscript </sub>
+<sup> superscript </sup>
+
+
+**Bold**
+*Italics*
+***Bold and Italics***
+
+[http://example.com]( Link )
+
+# Heading1
+## Heading2
+### Heading3
+#### Heading4
+##### Heading5
+###### Heading6
+
+"""
+
+	for i in l.get_tokens(text):
+		print(i)
