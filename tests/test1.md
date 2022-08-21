2bea5e1460b66b457076b8e18566590984180e47
Ayush K M <kmayushkm@gmail.com>
2022-08-21 19:35:00 +0530

    Changed tests

---
+++ tests/test1.md
@@ -0,0 +1,39 @@
+<!-- This
+is
+actually
+a comment-->
+
+`Single line code`
+
+```
+Multiline code
+
+code
+```
+
+line break<br>
+
+text
+
+> Quote
+
+<sub> subscript </sub>
+<sup> superscript </sup>
+
+
+**Bold**
+*Italics*
+***Bold and Italics***
+
+[http://example.com]( Link )
+
+![http://example.com/example.png]( This is an Image )
+
+# Heading1
+## Heading2
+### Heading3
+#### Heading4
+##### Heading5
+###### Heading6
+
+~~Striked Text~~
--- tests/test1.py
+++ tests/test1.py
@@ -7,49 +7,6 @@ from convmd import lexer
 
 if __name__ == "__main__":
 	l = lexer.MarkdownLexer()
-	text = """
-
-<!-- This
-is
-actually
-a comment-->
-
-`Single line code`
-
-```
-Multiline code
-
-code
-```
-
-line break<br>
-
-text
-
-> Quote
-
-<sub> subscript </sub>
-<sup> superscript </sup>
-
-
-**Bold**
-*Italics*
-***Bold and Italics***
-
-[http://example.com]( Link )
-
-![http://example.com/example.png]( This is an Image )
-
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
-
-~~Striked Text~~
-
-"""
-
+	text = (open("tests/test1.md", "r")).read()
 	for i in l.get_tokens(text):
 		print(i)
